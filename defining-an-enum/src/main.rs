bea66a9749d6d80c1988a82f1fb49f9195a95984
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-06-23 11:30:32 +0800

    feat(defining-an-enum): Learning use match to consume value of enum and if let syntax

--- defining-an-enum/src/main.rs
+++ defining-an-enum/src/main.rs
@@ -16,6 +16,20 @@ impl Animal {
     }
 }
 
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
     let ipv4 = IpAddr::V4(127, 0, 0, 1);
     let ipv6 = IpAddr::V6(String::from("::1"));
@@ -35,4 +49,42 @@ fn main() {
     let baz: Option<u32> = None;
 
     dbg!(foo, bar, baz);
+
+    let coin = Coin::Quarter(UsState::Alabama);
+    value_in_cents(&coin);
+    match_catch_all(&coin);
+    if_let(&coin);
+}
+
+fn value_in_cents(coin: &Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+/** 只想处理 enum 的部分 variants，忽略其他 variants */
+fn match_catch_all(coin: &Coin) {
+    match coin {
+        Coin::Quarter(state) => {
+            println!("[match_catch_all] - {:?}", state);
+        }
+        _ => (),
+    }
+}
+
+/** if let 可以看成是 match 的其中一种情况，当只想处理 match 的一种情况是适合使用，算是 match 的语法糖 */
+fn if_let(coin: &Coin) {
+    if let Coin::Quarter(state) = coin {
+        println!("[if_let] coin is Quarter - state is {:?}", state);
+    } else if let Coin::Dime = coin {
+        println!("[if_let] coin is Dime");
+    } else {
+        println!("[if_let] coin is other");
+    }
 }
