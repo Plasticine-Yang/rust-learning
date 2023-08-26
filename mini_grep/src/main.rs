2f6e81eaee4261af0c7becf7601b227c677b4bea
plasticine <975036719@qq.com>
2023-08-26 16:27:05 +0800

    refactor(mini_grep): split code into library crate

--- mini_grep/src/main.rs
+++ mini_grep/src/main.rs
@@ -1,4 +1,6 @@
-use std::{env, error::Error, fs, process};
+use std::{env, process};
+
+use mini_grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,34 +15,8 @@ fn main() {
 
     // 对于这种只关注 Result 的 Err 的场景，更适合用 if let 而不是 `unwrap_or_else`
     // 因为 Result 的 Ok 内的值永远是 unit type ()，对我们而言没有任何意义
-    if let Err(e) = run(config) {
+    if let Err(e) = mini_grep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
 }
