f422105fca3f968a4c8fe649163832ab544c9c9a
plasticine <975036719@qq.com>
2023-08-26 16:15:51 +0800

    feat(mini_grep): returning errors from the run function

--- mini_grep/src/main.rs
+++ mini_grep/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,11 +14,12 @@ fn main() {
     run(config);
 }
 
-fn run(config: Config) {
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
