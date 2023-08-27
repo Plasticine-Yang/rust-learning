b629404b5255d283cf7f067dfe073a4bcb4a2235
plasticine <975036719@qq.com>
2023-08-28 00:37:58 +0800

    chore(mini_grep): remove useless println

--- mini_grep/src/main.rs
+++ mini_grep/src/main.rs
@@ -10,9 +10,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     // 对于这种只关注 Result 的 Err 的场景，更适合用 if let 而不是 `unwrap_or_else`
     // 因为 Result 的 Ok 内的值永远是 unit type ()，对我们而言没有任何意义
     if let Err(e) = mini_grep::run(config) {
