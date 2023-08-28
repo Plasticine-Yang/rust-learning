5e91064a40d0e18a680a3f470191e1c6807b5a81
plasticine <975036719@qq.com>
2023-08-29 00:23:30 +0800

    refactor(mini_grep): printing errors to standard error

--- mini_grep/src/main.rs
+++ mini_grep/src/main.rs
@@ -6,14 +6,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        // eprintln! macro 会将字符串输出到 stderr 中
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     // 对于这种只关注 Result 的 Err 的场景，更适合用 if let 而不是 `unwrap_or_else`
     // 因为 Result 的 Ok 内的值永远是 unit type ()，对我们而言没有任何意义
     if let Err(e) = mini_grep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     };
 }
