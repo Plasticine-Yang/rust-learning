e018e0905272a0590b6f246066b09612b4c44b29
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-09-10 12:23:14 +0800

    refactor(smart-pointers): 抽离 ConsList enum 进行复用

--- smart-pointers/src/box_demo.rs
+++ smart-pointers/src/box_demo.rs
@@ -16,7 +16,7 @@
 
 #[cfg(test)]
 mod tests {
-    use std::fmt::Display;
+    use crate::shared::list::ConsList;
 
     #[test]
     fn first_try_of_box() {
@@ -26,48 +26,6 @@ mod tests {
 
     #[test]
     fn recursive_type() {
-        /// 对于类型中存在递归类型的场景，比如这里 `ConsList` 链表节点中有成员的值又是 `ConsList`，Rust 编译器无法在编译时知道该类型的大小
-        /// 这种时候就可以考虑用 `Box`
-        // enum ConsList {
-        //     Data(i32, ConsList),
-        //     Nil,
-        // }
-
-        enum ConsList {
-            Data(i32, Box<ConsList>),
-            Nil,
-        }
-
-        impl Display for ConsList {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                fn perform_unit_of_work(cons_list: &ConsList, f: &mut std::fmt::Formatter<'_>) {
-                    match cons_list {
-                        ConsList::Data(value, next) => {
-                            f.write_fmt(format_args!("{}", value))
-                                .expect("format data error");
-
-                            match **next {
-                                ConsList::Data(_, _) => {
-                                    f.write_str(" -> ").expect("format data error");
-                                    perform_unit_of_work(next, f);
-                                }
-                                ConsList::Nil => perform_unit_of_work(next, f),
-                            }
-                        }
-                        ConsList::Nil => (),
-                    }
-                }
-
-                f.write_str("[").expect("format start error");
-
-                perform_unit_of_work(self, f);
-
-                f.write_str("]").expect("format end error");
-
-                Ok(())
-            }
-        }
-
         let foo = ConsList::Data(
             1,
             Box::new(ConsList::Data(
