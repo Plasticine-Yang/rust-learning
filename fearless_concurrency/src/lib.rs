361f94d489b36c3230e81cee1dccf5de61eb14e7
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-09-18 17:25:34 +0800

    feat(fearless_concurrency): Using Threads to Run Code Simultaneously

---
+++ fearless_concurrency/src/lib.rs
@@ -0,0 +1,11 @@
+//! Fearless Concurrency
+//!
+//! Rust 中实现并发的四种方式：
+//!
+//! 1. [多线程](crate::using_threads_to_run_code_simultaneously)
+//! 2. 消息传递
+//! 3. 共享状态
+//! 4. Sync and Send
+//!
+
+pub mod using_threads_to_run_code_simultaneously;
