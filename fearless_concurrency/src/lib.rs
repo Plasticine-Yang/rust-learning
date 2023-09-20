1ad4d3648049a4fb7ea75fc940a426c5e6f77af3
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-09-20 20:17:19 +0800

    feat(fearless_concurrency): message passing

--- fearless_concurrency/src/lib.rs
+++ fearless_concurrency/src/lib.rs
@@ -2,10 +2,11 @@
 //!
 //! Rust 中实现并发的四种方式：
 //!
-//! 1. [多线程](crate::using_threads_to_run_code_simultaneously)
-//! 2. 消息传递
+//! 1. [多线程](crate::multi_threads)
+//! 2. [消息传递](crate::message_passing)
 //! 3. 共享状态
 //! 4. Sync and Send
 //!
 
-pub mod using_threads_to_run_code_simultaneously;
+pub mod message_passing;
+pub mod multi_threads;
---
+++ fearless_concurrency/src/message_passing.rs
@@ -0,0 +1,134 @@
+//! Using Message Passing to Transfer Data Between Threads
+
+#[cfg(test)]
+mod tests {
+    // mpsc - multiple producer, single consumer
+    use std::{sync::mpsc, thread, time::Duration};
+
+    /// 阻塞调用
+    #[test]
+    fn mpsc_blocking() {
+        // tx - transmitter 发送方 | rx - receiver 接收方
+        // 这种命名是历史原因导致的
+        let (tx, rx) = mpsc::channel();
+
+        // 子线程通过闭包捕获主线程上下文中的 tx 发送消息
+        thread::spawn(move || {
+            let value = String::from("[spawn thread] value");
+
+            for _ in 0..3 {
+                println!("[spawn thread] loading...");
+                thread::sleep(Duration::from_millis(300));
+            }
+
+            tx.send(value).unwrap();
+        });
+
+        // 主线程通过 rx 接收子线程传来的消息
+        let received_value = rx.recv().unwrap();
+        println!("[main thread] receive value: {}", received_value);
+    }
+
+    /// 非阻塞调用
+    #[test]
+    fn mpsc_non_blocking() {
+        let (tx, rx) = mpsc::channel();
+
+        thread::spawn(move || {
+            let value = String::from("[spawn thread] value");
+
+            for _ in 0..3 {
+                println!("[spawn thread] loading...");
+                thread::sleep(Duration::from_millis(300));
+            }
+
+            tx.send(value).unwrap();
+        });
+
+        for _ in 0..5 {
+            println!("[main thread] waiting for spawned thread message...");
+            thread::sleep(Duration::from_millis(500));
+
+            // 繁琐一点的写法
+            // match rx.try_recv() {
+            //     Ok(received_value) => {
+            //         println!("[main thread] receive value: {}", received_value);
+            //         break;
+            //     }
+            //     Err(_) => {
+            //         println!("[main thread] not receive value yet.",);
+            //     }
+            // }
+
+            // 更简洁的写法
+            if let Ok(received_value) = rx.try_recv() {
+                println!("[main thread] receive value: {}", received_value);
+                break;
+            } else {
+                println!("[main thread] not receive value yet.",);
+            }
+        }
+    }
+
+    /// 以 iterator 的方式消费多个到来的消息
+    #[test]
+    fn consume_message_with_iterator() {
+        let (tx, rx) = mpsc::channel();
+
+        thread::spawn(move || {
+            let values = vec![
+                String::from("many"),
+                String::from("messages"),
+                String::from("is"),
+                String::from("coming"),
+            ];
+
+            for value in values {
+                tx.send(value).unwrap();
+                thread::sleep(Duration::from_millis(300));
+            }
+        });
+
+        for received_value in rx {
+            println!("[main thread] receive message: {}", received_value)
+        }
+    }
+
+    #[test]
+    fn multiple_producers() {
+        let (tx, rx) = mpsc::channel();
+
+        let tx_cloned = tx.clone();
+
+        thread::spawn(move || {
+            let values = vec![
+                String::from("many"),
+                String::from("messages"),
+                String::from("is"),
+                String::from("coming"),
+            ];
+
+            for value in values {
+                tx.send(value).unwrap();
+                thread::sleep(Duration::from_millis(300));
+            }
+        });
+
+        thread::spawn(move || {
+            let values = vec![
+                String::from("and"),
+                String::from("more"),
+                String::from("messages"),
+            ];
+
+            for value in values {
+                tx_cloned.send(value).unwrap();
+                thread::sleep(Duration::from_millis(300));
+            }
+        });
+
+        for received_value in rx {
+            println!("[main thread] receive message: {}", received_value)
+        }
+    }
+}
