9e5dca8dc9239e502ccf3d495aa9d8f9d7532054
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-09-26 14:45:25 +0800

    feat(thumbor): 引入 http 服务器运行图片解析服务

--- thumbor/src/main.rs
+++ thumbor/src/main.rs
@@ -1,3 +1,44 @@
+use axum::{extract::Path, handler::get, http::StatusCode, Router};
+use percent_encoding::percent_decode_str;
+use serde::Deserialize;
+
 mod pb;
 
-fn main() {}
+use pb::*;
+
+// 参数使用 serde 做 Deserialize，axum 会自动识别并解析
+#[derive(Deserialize)]
+struct Params {
+    spec: String,
+    url: String,
+}
+
+#[tokio::main]
+async fn main() {
+    // 初始化 tracing
+    tracing_subscriber::fmt::init();
+
+    // 构建路由
+    let app = Router::new()
+        // `GET /` 会执行
+        .route("/image/:spec/:url", get(generate));
+
+    // 运行 web 服务器
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    tracing::debug!("Listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// basic handler that responds with a static string
+async fn generate(Path(Params { spec, url }): Path<Params>) -> Result<String, StatusCode> {
+    let url = percent_decode_str(&url).decode_utf8_lossy();
+    let spec: ImageSpec = spec
+        .as_str()
+        .try_into()
+        .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+    Ok(format!("url: {}\n spec: {:#?}", url, spec))
+}
